bfc078951a45c3bdad2e471dc139e1f3dac65965
HmDol <vtwin321@naver.com>
2024-11-19 15:52:11 +0900

    123

---
+++ Sorting/버블정렬.java
@@ -0,0 +1,29 @@
+package Sorting;
+
+import java.util.Scanner;
+
+public class 버블정렬 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = n - 1; i > 0; i--) {
+            for (int j = 0; j <= i - 1; j++) {
+                if (arr[j + 1] < arr[j]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+
+            }
+        }
+        for (int x : arr)
+            System.out.print(x + " ");
+    }
+}
